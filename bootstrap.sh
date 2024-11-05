86d98815631700217a85ab054c09877fd751d52a
Dave Wingard <dave-wingard@users.noreply.github.com>
2024-11-05 00:15:58 -0500

    simplified ssh key setup steps

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,13 +13,14 @@ sudo apt update ; sudo apt install -y git ansible rsync
 
 ### Copy ssh keys
 check_not_root
-rsync dave@nas01:~/setup/id_rsa* ~/
+
 mkdir ~/.ssh
-mv id_rsa* ~/.ssh
-chown -R dave:dave ~/.ssh
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/id_rsa
-chmod 644 ~/.ssh/id_rsa.pub
+rsync dave@nas01:~/setup/id_rsa* ~/.ssh
+
+sudo chown -R dave:dave ~/.ssh
+sudo chmod 700 ~/.ssh
+sudo chmod 600 ~/.ssh/id_rsa
+sudo chmod 644 ~/.ssh/id_rsa.pub
 
 ### Clone this repo
 mkdir ~/github
